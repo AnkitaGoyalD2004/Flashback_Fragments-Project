510d59dd7e17b822d8e9c04564dc43fd710c76f9
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-19 13:57:41 +0530

    setting the routes

--- server/index.js
+++ server/index.js
@@ -5,21 +5,20 @@ import cors from "cors";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI);
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+app.use(express.json({ limit: "30mb", extended: "true" }));
+app.use(express.urlencoded({ limit: "30mb", extended: "true" }));
+
 app.use(cors());
-const CONNECTION_URL =
-  "mongodb+srv://Dishu01:Abhay@1_@cluster0.kr6sa5z.mongodb.net/";
-const PORT = process.env.PORT || 5000;
 
-moongoose
-  .connect(CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(PORT, () => console.log(`server running on port : ${PORT}`))
-  )
-  .catch((error = console.log(error.message)));
-
-mongoose.set("useFindAndModify", false);
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDb");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
