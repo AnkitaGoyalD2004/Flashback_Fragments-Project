49b746ec692afe0bb17d7f1c35999f658c0b2688
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-19 13:02:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,3 +7,7 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
+app.use(cors());
+const CONNECTION_URL =
+  "mongodb+srv://Dishu01:Abhay@1_@cluster0.kr6sa5z.mongodb.net/";
+const PORT = process.env.PORT || 5000;
