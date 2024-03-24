1501303e1d0b0d20af5894ea061a72280ff10c5f
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-24 18:48:50 +0530

    Update connectDb.js

--- server/connection/connectDb.js
+++ server/connection/connectDb.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URI);
-    console.log("hello");
   } catch (error) {
     return console.log(error);
   }
