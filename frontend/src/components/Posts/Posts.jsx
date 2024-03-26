8f4469ec3967b472d565cdeb978f8eb34dfd836d
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-26 17:50:45 +0530

    components

---
+++ frontend/src/components/Posts/Posts.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SinglePosts from "../singlePosts/singlePosts";
+const Posts = () => {
+  return (
+    <div>
+      <h1>Posts</h1>
+      <SinglePosts></SinglePosts>
+    </div>
+  );
+};
+
+export default Posts;
