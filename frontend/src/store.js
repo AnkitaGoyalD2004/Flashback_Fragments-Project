a4f02462169da04bf88c022532ad90b7e121ea14
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-28 13:59:53 +0530

    store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,3 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import PostsSlice from "./features/Posts/PostsSlice";
 import UserSlice from "./features/User/UserSlice";
+
+export const store = configureStore({
+    middleware : (getDefaultMiddleware) => getDefaultMiddleware({
+
+    }),
+reducer:{
+postReducer:
+userState:
+    }
+})
