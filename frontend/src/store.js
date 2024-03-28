3d8d5bcbfe2f78ae2dc592ffe5d7f5fff4939866
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-28 14:56:58 +0530

    store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,11 +4,5 @@ import PostsSlice from "./features/Posts/PostsSlice";
 import UserSlice from "./features/User/UserSlice";
 
 export const store = configureStore({
-    middleware : (getDefaultMiddleware) => getDefaultMiddleware({
-
-    }),
-reducer:{
-postReducer:
-userState:
-    }
-})
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({}),
+});
