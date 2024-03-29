0ebea6869822b231df1c231d867cd9104a5962bc
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-29 12:26:54 +0530

    Form

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -28,48 +28,7 @@ const Form = () => {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      {currentPostId && <Alert variant="info">Editing a Memory</Alert>}
-      <Input
-        w="full"
-        name="creator"
-        placeholder="Creator"
-        value={postData.creator}
-        onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
-      />
-      <Input
-        w="full"
-        name="title"
-        placeholder="Title"
-        value={postData.title}
-        onChange={(e) => setPostData({ ...postData, title: e.target.value })}
-      />
-      <Textarea
-        w="full"
-        name="message"
-        placeholder="Message"
-        value={postData.message}
-        onChange={(e) => setPostData({ ...postData, message: e.target.value })}
-      />
-      <Input
-        w="full"
-        name="tags"
-        placeholder="Tags (separated by commas)"
-        value={postData.tags}
-        onChange={(e) =>
-          setPostData({ ...postData, tags: e.target.value.split(",") })
-        }
-      />
-      <div className="fileInput">
-        <input type="file" onChange={handleInput} />
-        <Button type="submit" color="primary" size="lg">
-          Submit
-        </Button>
-        <Button color="secondary" size="sm" onClick={clear}>
-          Clear
-        </Button>
-      </div>
-    </Form>
+   
   );
 };
 
