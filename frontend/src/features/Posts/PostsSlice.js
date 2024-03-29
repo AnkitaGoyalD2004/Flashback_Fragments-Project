f426cd1df617cedb1913b3ec5b8d0508e66d3fd0
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-03-29 06:02:16 +0530

    create a slice

--- frontend/src/features/Posts/PostsSlice.js
+++ frontend/src/features/Posts/PostsSlice.js
@@ -1,1 +1,23 @@
-export default PostsSlice.reducer;
+import { createSlice } from "@reduxjs/toolkit";
+
+const defaultState = {
+  posts: [],
+  currentId: null,
+};
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState: defaultState,
+  reducers: {
+    setAllPosts: (state, action) => {
+      state.posts = action.payload;
+    },
+    addPost: () => {},
+    selectPost: () => {},
+    updatePost: () => {},
+    deletePost: () => {},
+    increaseLike: () => {},
+  },
+});
+
+export default postsSlice;
