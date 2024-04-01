6efa7c9862a056a22bf76e8dfef0ea420603620f
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-04-01 10:26:06 +0530

    Updating Pages

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { NavBar } from "../../components2";
+import { Outlet } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <NavBar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Home;
