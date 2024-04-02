c7ac363788fc007c75d9857f27a916a459f1323d
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-04-02 14:05:05 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -34,7 +34,9 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToPink"> Sign In</Button>
+          <Button gradientDuoTone="purpleToPink" outline>
+            Sign In
+          </Button>
         </Link>
         <Navbar.Toggle />
       </div>
--- server/routes/posts.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { getPosts, createPost } from "../controllers/postsController.js";
-const postRouter = express.Router();
-
-postRouter.get("/", getPosts);
-postRouter.get("/", createPost);
-
-export default postRouter;
