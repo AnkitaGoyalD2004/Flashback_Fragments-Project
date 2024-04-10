cadb32f906592c311f9c43bf415ff4e4fb25d8aa
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-04-10 15:48:16 +0530

    complete sign-in page functionallity

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react"; // Correct hook import
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
   return <div>Sign In</div>;
